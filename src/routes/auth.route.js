5eba65b74aeca8c3b91217dd0b38373b7f9f94c8
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-25 12:26:15 +0530

    feat: user logout route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/auth.controller.js');
+const { login, logout } = require('../controllers/auth.controller.js');
+const { checkAuthentication } = require('../middlewares/auth.middleware.js');
 
 // Public route
 router.post('/login', login);
 
-
+// Protected route
+router.post('/logout', checkAuthentication, logout);
 
 module.exports = router;
