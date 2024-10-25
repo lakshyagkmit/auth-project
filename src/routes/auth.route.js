69ee828d6c06c512f8f275df230865105d086037
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-25 17:45:19 +0530

    feat: add register route in auth.route file

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,12 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { login, logout } = require('../controllers/auth.controller.js');
+const { register, login, logout } = require('../controllers/auth.controller.js');
 const { checkAuthentication } = require('../middlewares/auth.middleware.js');
-const { loginValidateSchema } = require('../validators/auth.validator.js');
+const { registerValidateSchema, loginValidateSchema } = require('../validators/auth.validator.js');
 
 // Public route
+router.post('/register', registerValidateSchema, register);
 router.post('/login', loginValidateSchema, login);
 
 // Protected route
